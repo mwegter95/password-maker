a42f413569d27f4527c2e2affc92d725d7f36cea
zweets <mwegter95@gmail.com>
2021-10-30 10:12:31 -0500

    added strings for characters, prompts for user

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialChars = "@#$%^&*"
+var finalString = ""
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,6 +14,27 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+
+function generatePassword() {
+  var lowCase = window.prompt("For your password, would you like lowercase letters to be included? (y/n)");
+  var upCase = window.prompt("For your password would you like uppercase letters to be included (y/n");
+  var specials = window.prompt("For your password, would you like special characters to be included? (y/n)");
+
+  if (lowCase = "y") {
+    finalString = finalString + lowercase
+  }
+  
+  if (upCase = "y") {
+    finalString = finalString + uppercase
+  }
+
+  if (specials = "y") {
+    finalString = finalString + specialChars
+  }
+  
+  console.log(finalString);
 }
 
 // Add event listener to generate button
